d5251f48efe5a990abd4a8da00e49004e205b943
Trikoz I <jleed@me.com>
2019-10-03 18:32:14 +0300

    Radio case

--- packages/docs/cases/Control/Radio/index.case.tsx
+++ packages/docs/cases/Control/Radio/index.case.tsx
@@ -1,24 +1,45 @@
 import Radio from '@flow-ui/core/control/Radio';
 import React, { Fragment, useState } from 'react';
 import { CaseProps } from "../../../plugins/CaseWrapper";
-import UICaseBlock from '@flow-ui/showcase/src/UI/components/UICaseBlock';
+import UICaseBlock, { fakeValue } from '@flow-ui/showcase/src/UI/components/UICaseBlock';
 import UICaseDocumentation from "@flow-ui/showcase/src/UI/components/UICaseDocumentation";
+import Flexbox from '@flow-ui/core/layout/Flexbox';
 
 export default (props: CaseProps) => {
 
     const RadioTypes = useState<any>({})
-
+    const [value, setValue] = useState(1)
     return (
         <Fragment>
             <UICaseBlock
                 title="Radio"
-                scope={{ Radio }}
+                subtitle="Recommended to use with useState for control state of Radio"
+                scope={{ Radio, Flexbox, setValue, value}}
                 props={{ RadioTypes }}
                 children={
-                    <Radio
-                        label="Check me"
-                        {...RadioTypes[0]}
-                    />
+                    <Flexbox column>
+                        <Radio
+                            checked={value === 1}
+                            label="Winter"
+                            m={"0.5rem"}
+                            {...RadioTypes[0]}
+                            onChange={fakeValue('() => setValue(1)')}
+                        />
+                        <Radio
+                            checked={value === 2}
+                            label="Summer"
+                            m={"0.5rem"}
+                            {...RadioTypes[0]}
+                            onChange={fakeValue('() => setValue(2)')}
+                        />
+                        <Radio
+                            checked={value === 3}
+                            label="Spring"
+                            m={"0.5rem"}
+                            {...RadioTypes[0]}
+                            onChange={fakeValue('() => setValue(3)')}
+                        />
+                    </Flexbox>
                 }
                 paths={[
                     `@flow-ui/core/control/Radio`,
