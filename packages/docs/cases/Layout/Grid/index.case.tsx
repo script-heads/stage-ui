651855edffda3989e428f56fd37d2075109b7b9f
Dmitry Beltyukov <beltdtr@gmail.com>
2019-10-04 13:16:25 +0300

    Grid case

---
+++ packages/docs/cases/Layout/Grid/index.case.tsx
@@ -0,0 +1,36 @@
+import Block from '@flow-ui/core/layout/Block';
+import Grid from '@flow-ui/core/layout/Grid';
+import React, { Fragment, useState } from 'react';
+import { CaseProps } from "../../../plugins/CaseWrapper";
+import UICaseBlock from '@flow-ui/showcase/src/UI/components/UICaseBlock';
+import UICaseDocumentation from "@flow-ui/showcase/src/UI/components/UICaseDocumentation";
+
+export default (props: CaseProps) => {
+
+    const GridTypes = useState<any>({})
+
+    return (
+        <Fragment>
+            <UICaseBlock
+                title="Flexbox"
+                subtitle={`Just a flexbox`}
+                scope={{ Grid, Block }}
+                props={{ GridTypes }}
+                children={(
+                    <Grid {...GridTypes[0]}>
+                        <Block p="2rem" m="1rem" surface="minor">Alice</Block>
+                        <Block p="2rem" m="1rem" surface="minor">Bob</Block>
+                        <Block p="2rem" m="1rem" surface="minor">Baker</Block>
+                        <Block p="2rem" m="1rem" surface="minor">Clark</Block>
+                        <Block p="2rem" m="1rem" surface="minor">Mason</Block>
+                        <Block p="2rem" m="1rem" surface="minor">Smith</Block>
+                    </Grid>
+                )}
+                paths={[
+                    `@flow-ui/core/layout/Flexbox`,
+                ]}
+            />
+            <UICaseDocumentation ns="FlexboxTypes" />
+        </Fragment>
+    )
+}
