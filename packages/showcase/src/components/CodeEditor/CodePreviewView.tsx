2b134157633c29a01dac9130e77b8f4d88d13b01
Trikoz I <jleed@me.com>
2019-10-09 17:59:03 +0300

    Brand new preview

--- packages/showcase/src/components/CodeEditor/CodePreviewView.tsx
+++ packages/showcase/src/components/CodeEditor/CodePreviewView.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live'
 import * as LabScope from '@flow-ui/lab'
 import * as CoreScope from '@flow-ui/core'
 import { Block } from '@flow-ui/core'
+import { transform} from '@babel/standalone'
+import { assign } from 'core-js/es/object';
+
+const _poly = { assign };
+
+Object.assign(window, {
+    React,
+    ...React,
+    ...CoreScope,
+    ...LabScope
+})
+
+// const evalCode = (code: string, scope: object) => {
+//   const scopeKeys = Object.keys(scope);
+//   const scopeValues = scopeKeys.map(key => scope[key]);
+//   const res = new Function('_poly', 'React', ...scopeKeys, code);
+//   return res(_poly, React, ...scopeValues);
+// };
+
 
 const GridBackground = (props: { set?: boolean, dark: boolean, children: any }) => {
     const gridColor1 = props.dark ? "#333333" : "#f4f4f4";
@@ -37,53 +55,21 @@ const GridBackground = (props: { set?: boolean, dark: boolean, children: any }) 
 const CodePreviewView = (props: { dark: boolean, code: string, showGrid: boolean }) => {
     let { code, dark } = props;
     const matchReturn = code.match('return')
+
+    let Render = null;
+    
+    try {
+        Render = eval(
+            transform(code, { presets: ["react"] }).code.split('export default ')[1].slice(0,-1) + '()'
+        )
+    } catch (error) {}
+
     if (matchReturn && matchReturn.index) {
-        code = code.slice(matchReturn.index)
-        let t = 0;
-        let cursor = 0;
-        let trim1 = -1;
-        let trim2 = -1;
-        while (cursor < code.length) {
-            if (code.charCodeAt(cursor) === 0x28) {
-                if (trim1 < 0) {
-                    trim1 = cursor
-                } else {
-                    t++
-                }
-            }
-            if (trim2 < 0 && code.charCodeAt(cursor) === 0x29) {
-                if (t) {
-                    t--
-                } else if (trim2 < 0) {
-                    trim2 = cursor
-                }
-            }
-            if (trim1 >= 0 && trim2 >= 0) {
-                code = code.slice(trim1 + 1, trim2)
-                break;
-            }
-            cursor++;
-        }
         return (
             <Block flex={1}>
                 <GridBackground set={props.showGrid} dark={dark}>
-                    <Block m="1rem">
-                        <LiveProvider
-                            code={code}
-                            language={"jsx"}
-                            scope={{
-                                ...(CoreScope || {}),
-                                ...(LabScope || {})
-                            }}
-                            children={(
-                                <LivePreview
-                                    style={{ 
-                                        position: "relative",
-                                        height: "100%" 
-                                    }}
-                                />
-                            )}
-                        />
+                    <Block m="1rem" css={{position:'relative', zIndex:1}}>
+                        {Render}
                     </Block>
                 </GridBackground>
             </Block>
