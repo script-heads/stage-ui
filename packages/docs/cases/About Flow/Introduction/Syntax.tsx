8f6dd30d97301c8d025cf7da162fd742044b4416
Trikoz I <jleed@me.com>
2019-10-10 13:11:57 +0300

    add syntax hl

---
+++ packages/docs/cases/About Flow/Introduction/Syntax.tsx
@@ -0,0 +1,30 @@
+import { useFlow } from "@flow-ui/core"
+import React from 'react'
+import { Prism, PrismLight } from 'react-syntax-highlighter'
+import light from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
+import dark from 'react-syntax-highlighter/dist/esm/styles/prism/vs-dark'
+
+const Syntax = (props: { code: any }) => {
+    const flow = useFlow()
+    const isDark = flow.theme.name.toUpperCase().match('DARK') ? true : false
+    const style = isDark ? dark : light
+    const HL = isDark ? Prism : PrismLight
+    const styles: any = {
+        borderRadius: flow.theme.radius.default,
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: flow.theme.color.lightest.css(),
+        background: flow.theme.color.surface.css(),
+    }
+    return (
+        <HL
+            showLineNumbers
+            language="typescript"
+            style={style}
+            children={props.code}
+            customStyle={styles}
+        />
+    )
+}
+
+export default Syntax
--- packages/docs/cases/About Flow/Introduction/index.case.tsx
+++ packages/docs/cases/About Flow/Introduction/index.case.tsx
@@ -1,5 +1,16 @@
-export const title = 'Hello :)'
+import React from 'react';
+import { Block, H1, T1, useFlow } from "@flow-ui/core";
+import Syntax from './Syntax';
+import ViewportCode from './viewport.raw';
+
+export const title = ' '
 
 export default () => (
-    null
+    <Block>
+        <H1 pb="1rem">Getting started</H1>
+        <T1>The main rule is you should wrap entire application with <b>Viewport</b>.</T1>
+        <T1>Viewport provides theme in all components, use <b>prop</b> theme to change it</T1>
+        <Syntax code={ViewportCode} />
+
+    </Block>
 )
