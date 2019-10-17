93ff72753d983762f6400e4489427396d9a98e6f
Trikoz I <jleed@me.com>
2019-10-17 18:21:36 +0300

    optimizations

--- packages/lab/layout/ScrollView/index.tsx
+++ packages/lab/layout/ScrollView/index.tsx
@@ -10,88 +10,109 @@ interface ScrollParams {
     deltaX: number
     deltaY: number
     preventDefault: () => void
+    stopPropagation: () => void
+    cursorHandle?: boolean
 }
+
 const ScrollView = (props: Types.Props, ref: any) => {
+    const memo: any = useMemo(() => ({
+        y: false,
+        x: false,
+        events: false,
+        yBar: null,
+        yThumb: null,
+        xBar: null,
+        xThumb: null,
+        container: null,
+        content: null,
+    }), []);
+
     const { mode = 'scroll' } = props
     const [active, setActive] = useState(false)
-    const [xDisplay, setXDisplay] = useState(true)
-    const [yDisplay, setYDisplay] = useState(true)
-    
+
     const styles = useStyles<Types.Styles, Types.Variants>(props, Styles)
 
     const { attributes } = useContainer(props)
 
-    let yBarRef: HTMLDivElement | null
-    let yThumbRef: HTMLSpanElement | null
-
-    let xBarRef: HTMLDivElement | null
-    let xThumbRef: HTMLSpanElement | null
-
-    let containerRef: HTMLDivElement | null
-    let contentRef: HTMLDivElement | null
-
     function updateScroll(e: ScrollParams) {
-        if (!containerRef || !contentRef) {
+        if (!memo.container || !memo.content) {
             return
         }
-        e.preventDefault()
 
         const updateY = () => {
-            if (!contentRef || !containerRef) return
+            if (!memo.content || !memo.container) return
 
-            const total = contentRef.offsetHeight
-            const content = containerRef.offsetHeight
+            const total = memo.content.offsetHeight
+            const content = memo.container.offsetHeight
+            
+            memo.yBar.style.visibility = total > content ? 'visible' : 'hidden'
 
-            setYDisplay(total > content)
-            if (total <= content || !yBarRef) {
-               return
+            if (total <= content) {
+                return
             }
-
-            const delta = e.deltaY
-            const offset = contentRef.offsetTop
 
             const min = 0
             const max = -(total - content)
-            const aspect = (content / total) * 100
+            const ratio = (content / total) * 100
+            const offset = memo.content.offsetTop
+            let delta = e.deltaY
 
-            let value = offset + -delta
+            if (e.cursorHandle) {
+                delta = delta * 100 / ratio
+            }
+
+            let value = offset - delta
 
             if (value > min) value = min
             if (value < max) value = max
 
-            contentRef!.style.top = value + 'px'
-            yBarRef!.style.height = content + 'px'
-            yThumbRef!.style.height = content * aspect / 100 + 'px'
-            yThumbRef!.style.transform = `translateY(${-(value * aspect / 100)}px)`
+            if (offset !== value) {
+                memo.content!.style.top = value + 'px'
+                memo.yBar!.style.height = content + 'px'
+                memo.yThumb!.style.height = content * ratio / 100 + 'px'
+                memo.yThumb!.style.transform = `translateY(${-(value * ratio / 100)}px)`
+
+                e.preventDefault()
+                e.stopPropagation()
+            }
         }
-        
+
         const updateX = () => {
-            if (!contentRef || !containerRef) return
+            if (!memo.content || !memo.container) return
 
-            const total = contentRef.offsetWidth
-            const content = containerRef.offsetWidth
+            const total = memo.content.offsetWidth
+            const content = memo.container.offsetWidth
 
-            setXDisplay(total > content)
+            memo.xBar.style.visibility = total > content ? 'visible' : 'hidden'
 
-            if (total <= content || !xBarRef) {
-               return
+            if (total <= content) {
+                return
             }
 
-            const delta = e.deltaX
-            const offset = contentRef.offsetLeft
             const min = 0
             const max = -(total - content)
-            const aspect = (content / total) * 100
+            const ratio = (content / total) * 100
+            const offset = memo.content.offsetLeft
+            let delta = e.deltaX
+
+            if (e.cursorHandle) {
+                delta = delta * 100 / ratio
+            }
 
             let value = offset + -delta
 
             if (value > min) value = min
             if (value < max) value = max
 
-            contentRef!.style.left = value + 'px'
-            xBarRef!.style.width = content + 'px'
-            xThumbRef!.style.width = content * aspect / 100 + 'px'
-            xThumbRef!.style.transform = `translateX(${-(value * aspect / 100)}px)`
+            if (offset !== value) {
+                memo.content!.style.left = value + 'px'
+                memo.xBar!.style.width = content + 'px'
+                memo.xThumb!.style.width = content * ratio / 100 + 'px'
+                memo.xThumb!.style.transform = `translateX(${-(value * ratio / 100)}px)`
+
+                e.preventDefault()
+                e.stopPropagation()
+            }
         }
 
         updateY()
@@ -106,6 +127,34 @@ const ScrollView = (props: Types.Props, ref: any) => {
         }
     }
 
+    function yMouseDown() {
+        memo.y = true
+    }
+
+    function xMouseDown() {
+        memo.x = true
+    }
+
+    function mouseUp() {
+        memo.y = false
+        memo.x = false
+    }
+
+    function mouseMove(e: MouseEvent) {
+        const deltaY = memo.y ? e.movementY : 0
+        const deltaX = memo.x ? e.movementX : 0
+
+        if (deltaX !== 0 || deltaY !== 0) {
+            updateScroll({
+                deltaX,
+                deltaY,
+                preventDefault: () => null,
+                stopPropagation: () => null,
+                cursorHandle: true
+            })
+        }
+    }
+
     useEffect(() => {
         setActive(mode == 'always' ? true : false)
     }, [mode])
@@ -114,55 +163,40 @@ const ScrollView = (props: Types.Props, ref: any) => {
         updateScroll({
             deltaX: 0,
             deltaY: 0,
-            preventDefault: () => null
+            preventDefault: () => null,
+            stopPropagation: () => null
         })
+
+        return () => {
+            window.removeEventListener('mouseup', mouseUp)
+            window.removeEventListener('mousemove', mouseMove)
+
+        }
     }, [])
 
+
+    function createRef(ref) {
+        if (ref && !memo.events) {
+            memo.events = true
+            window.addEventListener('mouseup', mouseUp, { passive: true })
+            window.addEventListener('mousemove', mouseMove, { passive: true })
+            memo.yThumb!.addEventListener('mousedown', yMouseDown, { passive: true })
+            memo.xThumb!.addEventListener('mousedown', xMouseDown, { passive: true })
+            memo.content!.addEventListener('wheel', updateScroll)
+        }
+        memo.container = ref
+    }
+
     return (
-        <div
-            {...attributes}
-            css={styles.container}
-            ref={ref => {
-                if (ref) {
-                    ref.removeEventListener('wheel', updateScroll)
-                    ref.addEventListener('wheel', updateScroll)
-                }
-                containerRef = ref
-            }}
-            children={(
-                <>
-                    <div
-                        css={styles.content}
-                        ref={ref => { contentRef = ref }}
-                        children={props.children}
-                    />
-                    {yDisplay && (
-                        <div
-                            css={styles.yBar({ active })}
-                            ref={ref => { yBarRef = ref }}
-                            children={(
-                                <span
-                                    css={styles.yThumb({ active })}
-                                    ref={ref => { yThumbRef = ref }}
-                                />
-                            )}
-                        />
-                    )}
-                    {xDisplay && (
-                        <div
-                            css={styles.xBar({ active })}
-                            ref={ref => { xBarRef = ref }}
-                            children={(
-                                <span
-                                    css={styles.xThumb({ active })}
-                                    ref={ref => { xThumbRef = ref }}
-                                />
-                            )}
-                        />
-                    )}
-                </>
-            )}
-        />
+        <div {...attributes} css={styles.container} ref={createRef}>
+            <div css={styles.content} ref={ref => memo.content = ref} children={props.children} />
+            <div css={styles.yBar({ active })} ref={ref => memo.yBar = ref}>
+                <span css={styles.yThumb({ active })} ref={ref => memo.yThumb = ref} />
+            </div>
+            <div css={styles.xBar({ active })} ref={ref => memo.xBar = ref}>
+                <span css={styles.xThumb({ active })} ref={ref => memo.xThumb = ref} />
+            </div>
+        </div>
     )
 }
 
