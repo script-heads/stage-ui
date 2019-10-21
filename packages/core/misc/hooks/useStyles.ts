3a17e953bf0d5f045364b4ebf518bf7af6189ab7
Dmitry Beltyukov <beltdtr@gmail.com>
2019-10-21 13:27:40 +0300

    useStyles variants composition fix

--- packages/core/misc/hooks/useStyles.ts
+++ packages/core/misc/hooks/useStyles.ts
@@ -27,24 +27,23 @@ const createStyles = <S>(
             FlowStyles[styleName] = (state) => {
 
                 const variant = (varaints) => {
-                    let variantStyles: any = {};
-                        for (const variantName of Object.keys(varaints)) {
-                            const variantValue = state[variantName];
-            
-                            if (typeof variantValue === 'string') {
-                                variantStyles = varaints[variantName][variantValue]
-                            }
-                            
-                            if (typeof variantValue === 'boolean' && variantValue === true) {
-                                variantStyles = varaints[variantName]
-                            }
-
-                            if (!Object.keys(state).includes(variantName)) {
-                                console.warn(`Can't find value of "${variantName}" variant in "${styleName}" style. Function has been called with:`, state)
-                            }
-                            
-                            return variantStyles
+                    let variantStyles: Global.EmotionStyles = [];
+                    
+                    for (const variantName of Object.keys(varaints)) {
+                        const variantValue = state[variantName];
+        
+                        if (typeof variantValue === 'string') {
+                            variantStyles.push(varaints[variantName][variantValue])
                         }
+                        
+                        if (typeof variantValue === 'boolean' && variantValue === true) {
+                            variantStyles.push(varaints[variantName])
+                        }
+
+                        if (!Object.keys(state).includes(variantName)) {
+                            console.warn(`Can't find value of "${variantName}" variant in "${styleName}" style. Function has been called with:`, state)
+                        }
+                    }
                     return variantStyles
                 }
                 
