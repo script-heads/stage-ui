93175d834547cbff665a6f97fcfe427d665d1b1e
Dmitry Beltyukov <beltdtr@gmail.com>
2019-10-28 15:18:26 +0300

    Drop scroll fix and improve preformance

--- packages/core/layout/Drop/index.tsx
+++ packages/core/layout/Drop/index.tsx
@@ -8,24 +8,114 @@ import dropStyles from './styles'
 const Drop = (props: Types.Props, ref) => {
 
     const { attributes } = useContainer(props)
-    const { children, target: targetRef, onClickOutside, distance = 0 } = props
+    const { children, target: targetRef, onClickOutside, distance = 0, align, justify } = props
     const styles = useStyles<Types.Styles>(props, dropStyles, 'Drop')
     const dropRef = useRef<HTMLDivElement>(null)
+    
+    let getTopCoord = (tr, dr) => toStyle(tr.bottom + distance)
+    let getLeftCoord = (tr, dr) => toStyle((tr.left + tr.width / 2) - dr.width / 2)
+
+    switch (align) {
+        case 'top':
+            getTopCoord = (tr, dr) => toStyle(tr.top - dr.height - distance)
+            switch (justify) {
+                case 'start':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left)
+                    break
+                case 'center':
+                    getLeftCoord = (tr, dr) => toStyle((tr.left + tr.width / 2) - dr.width / 2)
+                    break
+                case 'end':
+                    getLeftCoord = (tr, dr) => toStyle(tr.right - dr.width)
+                    break
+                case 'start-outside':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left - tr.width)
+                    break
+                case 'end-outside':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left + tr.width)
+                    break
+            }
+            break
+        case 'bottom':
+            getTopCoord = (tr, dr) => toStyle(tr.bottom + distance)
+            switch (justify) {
+                case 'start':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left)
+                    break
+                case 'center':
+                    getLeftCoord = (tr, dr) => toStyle((tr.left + tr.width / 2) - dr.width / 2)
+                    break
+                case 'end':
+                    getLeftCoord = (tr, dr) => toStyle(tr.right - dr.width)
+                    break
+                case 'start-outside':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left - tr.width)
+                    break
+                case 'end-outside':
+                    getLeftCoord = (tr, dr) => toStyle(tr.left + tr.width)
+                    break
+            }
+            break
+        case 'left':
+            getLeftCoord = (tr, dr) => toStyle(tr.left - dr.width - distance)
+            switch (justify) {
+                case 'end':
+                    getTopCoord = (tr, dr) => toStyle(tr.top)
+                    break
+                case 'center':
+                    getTopCoord = (tr, dr) => toStyle((tr.top + tr.height / 2) - dr.height / 2)
+                    break
+                case 'start':
+                    getTopCoord = (tr, dr) => toStyle(tr.bottom - dr.height)
+                    break
+                case 'start-outside':
+                    getTopCoord = (tr, dr) => toStyle(tr.top - tr.height)
+                    break
+            }
+            break
+        case 'right':
+            getLeftCoord = (tr, dr) => toStyle(tr.right + distance)
+            switch (justify) {
+                case 'end':
+                    getTopCoord = (tr, dr) => toStyle(tr.top)
+                    break
+                case 'center':
+                    getTopCoord = (tr, dr) => toStyle((tr.top + tr.height / 2) - dr.height / 2)
+                    break
+                case 'start':
+                    getTopCoord = (tr, dr) => toStyle(tr.bottom - dr.height)
+                    break
+                case 'start-outside':
+                    getTopCoord = (tr, dr) => toStyle(tr.top - tr.height)
+                    break
+            }
+            break
+    }
 
     useImperativeHandle(ref, () => {
         return dropRef.current
     })
 
     useEffect(() => {
+        const style = dropRef.current?.style
+
+        if (props.stretchHeight && style) {
+            style.height = toStyle(targetRef.current?.getBoundingClientRect().height)
+        }
+
+        if (props.stretchWidth && style) {
+            style.width = toStyle(targetRef.current?.getBoundingClientRect().width)
+        }
+        
         if (props.visibility != 'hidden' && props.display != 'none') {
             setPosition()
-            document.addEventListener('scroll', setPosition, true)
+            document.addEventListener('wheel', setPosition, true)
             document.addEventListener('mouseup', handleClickOutside)
             window.addEventListener('resize', setPosition)
 
         }
         return () => {
-            document.removeEventListener('scroll', setPosition, true)
+            document.removeEventListener('wheel', setPosition, true)
             document.removeEventListener('mouseup', handleClickOutside)
             window.removeEventListener('resize', setPosition)
         }
@@ -38,104 +128,14 @@ const Drop = (props: Types.Props, ref) => {
     }
 
     function setPosition() {
-        if (targetRef && targetRef.current && dropRef.current) {
-            const tr = targetRef.current?.getBoundingClientRect()
+        if (targetRef.current && dropRef.current) {
+            const tr = targetRef.current.getBoundingClientRect()
             const dr = dropRef.current.getBoundingClientRect()
             const style = dropRef.current.style
 
-            const {
-                align = 'bottom',
-                justify = 'center'
-            } = props
-
-            style.visibility = 'visible'
-            style.top = toStyle(tr.bottom + distance)
-            style.left = toStyle((tr.left + tr.width / 2) - dr.width / 2)
-
-            switch (align) {
-                case 'top':
-                    style.top = toStyle(tr.top - dr.height - distance)
-                    switch (justify) {
-                        case 'start':
-                            style.left = toStyle(tr.left)
-                            break
-                        case 'center':
-                            style.left = toStyle((tr.left + tr.width / 2) - dr.width / 2)
-                            break
-                        case 'end':
-                            style.left = toStyle(tr.right - dr.width)
-                            break
-                        case 'start-outside':
-                            style.left = toStyle(tr.left - tr.width)
-                            break
-                        case 'end-outside':
-                            style.left = toStyle(tr.left + tr.width)
-                            break
-                    }
-                    break
-                case 'bottom':
-                    style.top = toStyle(tr.bottom + distance)
-                    switch (justify) {
-                        case 'start':
-                            style.left = toStyle(tr.left)
-                            break
-                        case 'center':
-                            style.left = toStyle((tr.left + tr.width / 2) - dr.width / 2)
-                            break
-                        case 'end':
-                            style.left = toStyle(tr.right - dr.width)
-                            break
-                        case 'start-outside':
-                            style.left = toStyle(tr.left - tr.width)
-                            break
-                        case 'end-outside':
-                            style.left = toStyle(tr.left + tr.width)
-                            break
-                    }
-                    break
-                case 'left':
-                    style.left = toStyle(tr.left - dr.width - distance)
-                    switch (justify) {
-                        case 'end':
-                            style.top = toStyle(tr.top)
-                            break
-                        case 'center':
-                            style.top = toStyle((tr.top + tr.height / 2) - dr.height / 2)
-                            break
-                        case 'start':
-                            style.top = toStyle(tr.bottom - dr.height)
-                            break
-                        case 'start-outside':
-                            style.top = toStyle(tr.top - tr.height)
-                            break
-                    }
-                    break
-                case 'right':
-                    style.left = toStyle(tr.right + distance)
-                    switch (justify) {
-                        case 'end':
-                            style.top = toStyle(tr.top)
-                            break
-                        case 'center':
-                            style.top = toStyle((tr.top + tr.height / 2) - dr.height / 2)
-                            break
-                        case 'start':
-                            style.top = toStyle(tr.bottom - dr.height)
-                            break
-                        case 'start-outside':
-                            style.top = toStyle(tr.top - tr.height)
-                            break
-                    }
-                    break
-            }
-
-            if (props.stretchHeight) {
-                style.height = toStyle(tr.height)
-            }
-
-            if (props.stretchWidth) {
-                style.width = toStyle(tr.width)
-            }
+            style.visibility = ''
+            style.top = getTopCoord(tr, dr)
+            style.left = getLeftCoord(tr, dr)
         }
     }
 
