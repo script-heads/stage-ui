28b220cbafb6f646b474d33d59cf21652241ed93
Dmitry Beltyukov <beltdtr@gmail.com>
2019-12-05 16:28:39 +0300

    Fix types layout

--- packages/showcase/src/components/UIDocumentation/Interface.tsx
+++ packages/showcase/src/components/UIDocumentation/Interface.tsx
@@ -1,8 +1,4 @@
-import Divider from '@flow-ui/core/content/Divider'
-import Icon from '@flow-ui/core/content/Icon'
-import { Header, Text } from '@flow-ui/core'
-import Block from '@flow-ui/core/layout/Block'
-import Flexbox from '@flow-ui/core/layout/Flexbox'
+import {Header, Text, Divider, Icon, Flexbox, Block, Paragraph} from '@flow-ui/core'
 import React, { Fragment, useState, useMemo } from 'react'
 import {TypeInterfaceChild, TypeInterface} from '.'
 export const typedoc = require('../../../generated/types.json')
@@ -83,80 +79,54 @@ const Interface = (props: {data: TypeInterface, cutTypes?: string[]}) => {
 
 const Type = (props: { type: TypeInterfaceChild, last: boolean} ) => {
     const {type, last} = props
-    let color = '#f3f3f3'
-    if (type.type === 'reference') {
-        color = '#fadeff'
-    }
-    if (type.type === 'stringLiteral') {
-        color = '#ffdede'
-    }
-    if (type.type === 'intrinsic') {
-        color = '#deffe4'
-    }
-    if (type.type === 'reflection') {
-        color = '#fdffde'
-    }
     return (
         <Block key={type.id}>
-            <Block mt="1rem" mb="1rem" ml=".5rem">
+            <Block mt="1rem" mb=".75rem" ml=".5rem">
                 <Flexbox>
-                    <Text 
+                    <Text
+                        mr=".5rem"
+                        css={{whiteSpace: 'nowrap'}} 
                         flex={1}>
-                        &#8226;&ensp;{type.name+(type.isOptional ? '?' : '')}
+                        &bull;&ensp;
+                        {type.deprecated !== void 0 && '[Deprecated] '}
+                        {type.name}
+                        {(type.isOptional ? '?' : '')}
                     </Text>
-                    {Array.isArray(type.values)
-                        && type.values.map(value =>
-                            <Text 
-                                size={2}
-                                key={value}
-                                // background={color}
-                                p={'0.125rem'}
-                                pl={'0.35rem'}
-                                pr={'0.35rem'}
-                                textColor={c => c.light.css()}
-                                // css={{
-                                //     color: '#000', margin: '0 2px',
-                                //     borderRadius: '4px'
-                                // }}
-                                children={`${value}`}
-                            />
-                        )}
-                    {!Array.isArray(type.values) && (
-                        <Text 
-                            size={2}
-                            color={c => c.hard.css()}
-                            children="Not documented yet"
-                        />
-                    )}
-                    {type.deprecated !== void 0 && (
-                        <Text 
-                            size={2}
-                            p={'0.125rem'}
-                            pl={'0.35rem'}
-                            pr={'0.35rem'}
-                            mr={'0.25rem'}
-                            children={'Deprecated'}
-                            background={'#000'}
-                            color={'#fff'}
-                            css={{ borderRadius: '0.35rem' }}
-                        />
-                    )}
+                    <Flexbox wrap="wrap" justifyContent="flex-end">
+                        {Array.isArray(type.values)
+                            ? type.values.map(value =>
+                                <Text
+                                    h="fit-content"
+                                    size={3}
+                                    key={value}
+                                    p=".25rem 0.5rem"
+                                    mx=".125rem"
+                                    mb=".25rem"
+                                    backgroundColor={c => c.lightest.css()}
+                                    css={{borderRadius: '.25rem'}}
+                                    children={`${value}`}
+                                />
+                            )
+                            : (
+                                <Text
+                                    size={2}
+                                    color={c => c.hard.css()}
+                                    children="Not documented yet"
+                                />
+                            )
+                        }
+                    </Flexbox>                        
                 </Flexbox>
+                <Paragraph
+                    ml="1rem"
+                    mb=".25rem"
+                    display="block"
+                    size={2} 
+                    color={c => c.hard.css()} 
+                    children={type.comment}
+                />
             </Block>
-            {typeof type.comment === 'string' && (
-                <Fragment>
-                    <Block m="1rem" mt="0">
-                        {type.comment.split('\n').map((text, index) => (
-                            <Text size={2} color={c => c.hard.css()} key={index}>{text}</Text>
-                        ))}
-                    </Block>
-                </Fragment>
-            )}
-            {
-                !last && (
-                    <Divider />
-                )
-            }
+            {!last && <Divider />}
         </Block>
     )
 }
