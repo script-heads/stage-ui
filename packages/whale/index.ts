34a7811f24fd71777c10966d6030209e69d12248
Dmitry Beltyukov <beltdtr@gmail.com>
2020-01-16 12:21:34 +0300

    Create whale index

---
+++ packages/whale/index.ts
@@ -0,0 +1,7 @@
+export {default as Provider} from './components/Provider'
+
+export {default as useTheme} from './hooks/useTheme'
+export {default as useComponent} from './hooks/useComponent'
+
+export {default as colorProp} from './utils/colorProp'
+export {default as createTheme} from './utils/createTheme'
