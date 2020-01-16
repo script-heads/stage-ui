65b88674f77ec550d9fd95a79d2ab163c2f0b146
Dmitry Beltyukov <beltdtr@gmail.com>
2020-01-16 16:38:18 +0300

    Fix whale error

--- packages/whale/hooks/useTheme.ts
+++ packages/whale/hooks/useTheme.ts
@@ -3,6 +3,6 @@ import { Context } from '../components/Provider'
 
 export default () => {
     const theme = useContext(Context)
-    if (Object.keys(theme).length > 0) console.error('Whale error: Theme not provided')
+    if (Object.keys(theme).length === 0) console.error('Whale error: Theme not provided')
     return (theme)
 }
