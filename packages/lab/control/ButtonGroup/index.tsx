5671b453ae5f1359debe916172d9189b70788cf1
Илья Трикоз <i.trikoz@abr-region.ru>
2020-01-17 12:28:32 +0300

    fix ButtonGroup props forward

--- packages/lab/control/ButtonGroup/index.tsx
+++ packages/lab/control/ButtonGroup/index.tsx
@@ -4,20 +4,20 @@ import ButtonGroupTypes from './types'
 
 const ButtonGroup = (props: ButtonGroupTypes.Props, ref: any) => {
     const { children } = props
-    const childs = Array.isArray(children) ? children : [children];
+    const childs = Array.isArray(children) ? children : [children]
     return (
-        <Flexbox>
+        <Flexbox {...props}>
             {childs.map((child, index) => {
                 const style: React.CSSProperties = {}
                 if (index !== childs.length -1) {
-                    style.borderTopRightRadius = 0;
-                    style.borderBottomRightRadius = 0;
-                    style.borderRightWidth = 0;
+                    style.borderTopRightRadius = 0
+                    style.borderBottomRightRadius = 0
+                    style.borderRightWidth = 0
                 }
                 if (index !== 0) {
-                    style.borderTopLeftRadius = 0;
-                    style.borderBottomLeftRadius = 0;
-                    style.borderLeftWidth = 0;
+                    style.borderTopLeftRadius = 0
+                    style.borderBottomLeftRadius = 0
+                    style.borderLeftWidth = 0
                 }
                 return (
                     <Button
