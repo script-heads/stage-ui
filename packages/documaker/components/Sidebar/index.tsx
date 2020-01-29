c5a047bcd62d46cdc6cb927c7e4b525f2e697ab2
Илья Трикоз <i.trikoz@abr-region.ru>
2020-01-29 13:57:26 +0300

    add search

--- packages/documaker/components/Sidebar/index.tsx
+++ packages/documaker/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { PagesType, PageType } from '../../core'
-import { Text, Menu, Block, Icon, Flexbox } from '@flow-ui/core'
+import { Text, TextField, Menu, Block, Icon, Flexbox } from '@flow-ui/core'
 import MenuTypes from '@flow-ui/core/control/Menu/types'
 import { Fragment, useState } from 'react'
 import WhaleTypes from '@flow-ui/whale/types'
@@ -16,9 +16,15 @@ const Sidebar = (props: SidebarProps) => {
 
 	const { pages, currentPage, onChange } = props
 	const [visibility, setVisibility] = useState<boolean>(false)
+	const [search, setSearch] = useState('')
 
 	const getMenuItems = (section: PageType[]): MenuTypes.Item[] =>
-		section.map(page => ({
+		section.filter(page => {
+			if (search) {
+				return !!page.title.toUpperCase().match(search.toUpperCase())
+			}
+			return true
+		}).map(page => ({
 			css: {
 				fontWeight: '700',
 				marginLeft: '1.5rem',
@@ -49,11 +55,31 @@ const Sidebar = (props: SidebarProps) => {
 						}
 					]
 				})}>
+
+				<TextField
+					size="s"
+					decoration="underline"
+					ml="1.5rem"
+					mb="1.5rem"
+					leftChild={
+						<Icon type={t => t.outline.search} />
+					}
+					placeholder="Search"
+					value={search}
+					onChange={e => {
+						setSearch(e.target.value)
+					}}
+					clearable
+				/>
 				{Object.keys(pages).map((section, index) => {
 					if (pages[section].length) {
+						const menuItems = getMenuItems(pages[section])
+						if (menuItems.length === 0) {
+							return null
+						}
 						return (
-							<Block key={'section-' + index} mb="1.5rem">
-								{section != 'Index' &&
+							<Block key={'section-' + index}>
+								{search === '' && section != 'Index' &&
 									<Text
 										color={c => c.light.css()}
 										children={section}
@@ -66,6 +92,7 @@ const Sidebar = (props: SidebarProps) => {
 									/>
 								}
 								<Menu
+									mb="1.5rem"
 									value={currentPage.url || -1}
 									decoration="color"
 									direction="column"
@@ -73,7 +100,7 @@ const Sidebar = (props: SidebarProps) => {
 										setVisibility(false)
 										onChange(value.toString())
 									}}
-									items={getMenuItems(pages[section])}
+									items={menuItems}
 								/>
 							</Block>
 						)
