e4b0ced21d0f154116c0e4fca3267bf7624b1c57
Илья Трикоз <i.trikoz@abr-region.ru>
2020-02-05 18:24:30 +0300

    memo styles

--- packages/whale/hooks/useComponent.ts
+++ packages/whale/hooks/useComponent.ts
@@ -6,74 +6,80 @@ import useTheme from './useTheme'
 import { useMemo } from 'react'
 
 interface Options<S> {
-    props, 
+    props,
     styles: WhaleTypes.Styles<S> | WhaleTypes.CreateStyles<S>,
-    styleProps?: {[K in keyof S]?: (keyof InjectedStyles)[]} 
+    styleProps?: { [K in keyof S]?: (keyof InjectedStyles)[] }
     mouseFocus?: boolean,
     focusDecoration?: boolean
 }
 
 const useComponent = <S, P>(overrideName: string, options: Options<S>, params = {}) => {
-    
-    const { props, mouseFocus, focusDecoration, styleProps } = options
-    const cs: WhaleTypes.ComponentStyles<S> = {} as WhaleTypes.ComponentStyles<S>
 
-    const theme = useMemo(() => useTheme(), [])
+    const { props, mouseFocus, focusDecoration, styleProps } = options
+
+    const theme = useTheme()
     
     const { attributes, events, focus } = attributeProps(props, theme, mouseFocus, focusDecoration)
 
-    const styles = typeof options.styles === 'function'
-        ? options.styles(props, theme, params)
-        : options.styles
+    const cs = useMemo(() => {
+        const cs: WhaleTypes.ComponentStyles<S> = {} as WhaleTypes.ComponentStyles<S>
 
-    const themeOverrides = overrideName && theme.overrides[overrideName]
-    
-    Object.keys(styles).map(styleName => {
-        if (typeof styles[styleName] === 'function') {
-            cs[styleName] = (state) => {
+        const styles = typeof options.styles === 'function'
+            ? options.styles(props, theme, params)
+            : options.styles
 
-                const variant = (varaints) => {
-                    let variantStyles: EmotionStyles = []
-                    
-                    for (const variantName of Object.keys(varaints)) {
-                        const variantValue = state[variantName]
-        
-                        if (typeof variantValue === 'string') {
-                            variantStyles.push(varaints[variantName][variantValue])
-                        }
-                        
-                        if (typeof variantValue === 'boolean' && variantValue === true) {
-                            variantStyles.push(varaints[variantName])
-                        }
+        const themeOverrides = overrideName && theme.overrides[overrideName]
 
-                        if (!Object.keys(state).includes(variantName)) {
-                            console.warn(
-                                `Can't find value of "${variantName}" variant in "${styleName}" style. 
-                                Function has been called with:`, 
-                                state
-                            )
+        Object.keys(styles).map(styleName => {
+            if (typeof styles[styleName] === 'function') {
+                cs[styleName] = (state) => {
+
+                    const variant = (varaints) => {
+                        let variantStyles: EmotionStyles = []
+
+                        for (const variantName of Object.keys(varaints)) {
+                            const variantValue = state[variantName]
+
+                            if (typeof variantValue === 'string') {
+                                variantStyles.push(varaints[variantName][variantValue])
+                            }
+
+                            if (typeof variantValue === 'boolean' && variantValue === true) {
+                                variantStyles.push(varaints[variantName])
+                            }
+
+                            if (!Object.keys(state).includes(variantName)) {
+                                console.warn(
+                                    `Can't find value of "${variantName}" variant in "${styleName}" style. 
+                                    Function has been called with:`,
+                                    state
+                                )
+                            }
                         }
+                        return variantStyles
                     }
-                    return variantStyles
+
+                    return css(
+                        styles[styleName](variant),
+                        themeOverrides?.[styleName]?.(variant),
+                        props.styles?.[styleName]?.(variant),
+                        getStyleProps(props, theme, styleProps?.[styleName])
+                    )
                 }
-                
-                return css(
-                    styles[styleName](variant), 
-                    themeOverrides?.[styleName]?.(variant),
-                    props.styles?.[styleName]?.(variant),
+            } else {
+                cs[styleName] = css(
+                    styles[styleName],
+                    themeOverrides?.[styleName],
+                    props.styles?.[styleName],
                     getStyleProps(props, theme, styleProps?.[styleName])
                 )
             }
-        } else {
-            cs[styleName] = css(
-                styles[styleName], 
-                themeOverrides?.[styleName],
-                props.styles?.[styleName],
-                getStyleProps(props, theme, styleProps?.[styleName])
-            )
-        }     
-    })
-    
+        })
+        
+        return cs
+
+    }, [props])
+
     return { cs, attributes, events, focus }
 }
 
