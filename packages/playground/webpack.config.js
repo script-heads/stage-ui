8721f3af5d47b764dde81dd017aa3923f3494662
Dmitry Beltyukov <beltdtr@gmail.com>
2020-02-14 11:11:23 +0300

    Build importPlugin fix

--- packages/playground/webpack.config.js
+++ packages/playground/webpack.config.js
@@ -112,8 +112,9 @@ if (process.env.DEBUG === 'true') {
  * Настройки для аналитики
  */
 if (process.env.ANALYZE === 'true') {
-    config.optimization = {}
-    config.optimization.concatenateModules = false
+    config.optimization = {
+        concatenateModules: false
+    }
     plugins.push(new BundleAnalyzerPlugin())
 }
 
