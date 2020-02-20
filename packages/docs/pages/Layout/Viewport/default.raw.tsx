b03f8bd421b9963f8102db52c6dd4cc9f87f62f1
Илья Трикоз <i.trikoz@abr-region.ru>
2020-02-20 18:20:50 +0300

    Viewport example

--- packages/docs/pages/Layout/Viewport/default.raw.tsx
+++ packages/docs/pages/Layout/Viewport/default.raw.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Viewport } from '@flow-ui/core';
+import React from 'react'
+import { Viewport } from '@flow-ui/core'
 
 export default () => {
     return (
-        <Viewport />
+        <Viewport>
+            Viewport provide theme
+            to all Flow components
+        </Viewport>
     )
 }
