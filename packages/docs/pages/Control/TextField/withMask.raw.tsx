24054b110e949a3f0c5d8c17521e638bfb1dddd5
Илья Трикоз <i.trikoz@abr-region.ru>
2020-02-27 11:24:11 +0300

    fix gen

--- packages/docs/pages/Control/TextField/withMask.raw.tsx
+++ packages/docs/pages/Control/TextField/withMask.raw.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { TextField } from '@flow-ui/core';
+import React from 'react'
+import { TextField } from '@flow-ui/core'
 
 export default () => {
     return (
         <TextField
             label={'Phone'}
-            placeholder='Please enter your phone'
-            masked={{
-                mask: "+7 (000) 000-00-00"
-            }}
+            placeholder="Please enter your phone"
         />
     )
 }
