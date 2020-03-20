ee6062eee065866cfebe15e84951b3a3df02f9c9
Илья Трикоз <i.trikoz@abr-region.ru>
2020-03-20 10:08:23 +0300

    p/m check undefined

--- packages/playground/index.tsx
+++ packages/playground/index.tsx
@@ -1,53 +1,14 @@
 import { Block, Viewport, TextField, Button, ScrollView } from '@flow-ui/core'
-import Person from '@flow-ui/core/icons/svg/person'
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const BlockLutiy = () => {
-	return (
-		<div>
-			{
-				'-'.repeat(0).split('-').map((_, index) => (
-					<Block key={index} >{index}</Block>
-				))
-			}
-		</div>
-	)
-}
-const Test = () => {
-	React.useEffect(() => {
-		return () => {
-			console.log(1)
-		}
-	},[])
-	return (
-		<span>
-			<div>
-				<div>
-					<Person />
-				</div>
-			</div>
-		</span>
-	)
-}
 const Playground = () => {
 	const [value, setValue] = useState('')
 	return (
 		<Viewport>
-			<ScrollView w="400px" h="400px" mode="always">
-				<div
-				css={{
-					background:'red',
-					width: '1000px',
-					height: '1000px'
-				}}
-				/>
-			</ScrollView>
-			{/* <TextField 
-				leftChild={<Test />}
-				value={value} 
-				onChange={(e) => setValue(e.target.value)}
-			/> */}
+			<Block m={undefined}>
+				1
+			</Block>
 		</Viewport>
 	)
 }
--- packages/whale/utils/attachProps/style/resolvers/paddingMargin.ts
+++ packages/whale/utils/attachProps/style/resolvers/paddingMargin.ts
@@ -5,30 +5,31 @@ import WhalePropsTypes from '../../types'
  * into single css string
  */
 const resolver: WhalePropsTypes.StyleResolver = (params) => {
-    let { propName: n, propValue, ctx, theme } = params,
-        k = n[0] == 'p' ? 'padding' : 'margin',
-        x = n[1] == 'x', y = n[1] == 'y', 
-        t = n[1] == 't', b = n[1] == 'b', 
-        l = n[1] == 'l', r = n[1] == 'r' 
+    if (params.propValue) {
+        let { propName: n, propValue, ctx, theme } = params,
+            k = n[0] == 'p' ? 'padding' : 'margin',
+            x = n[1] == 'x', y = n[1] == 'y',
+            t = n[1] == 't', b = n[1] == 'b',
+            l = n[1] == 'l', r = n[1] == 'r'
 
-    if (typeof propValue === 'number') {
-        propValue = propValue.toString() + 'px'
-    }
+        if (typeof propValue === 'number') {
+            propValue = propValue.toString() + 'px'
+        }
+        const v = propValue.split(' ').map(value => theme.spacing[value] || value)
 
-    const v = propValue.split(' ').map(value => theme.spacing[value] || value)
-    
-    if (n.length == 1) {
-        if (v[1] == void 0) v[1] = v[0]
-        if (v[2] == void 0) v[2] = v[0]
-        if (v[3] == void 0) v[3] = v[1]
-        ctx[k] = v
-    } else {
-        if (t || y) ctx[k][0] = v[0]
-        if (r || x) ctx[k][1] = v[0]
-        if (b || y) ctx[k][2] = v[1] || v[0]
-        if (l || x) ctx[k][3] = v[1] || v[0]
+        if (n.length == 1) {
+            if (v[1] == void 0) v[1] = v[0]
+            if (v[2] == void 0) v[2] = v[0]
+            if (v[3] == void 0) v[3] = v[1]
+            ctx[k] = v
+        } else {
+            if (t || y) ctx[k][0] = v[0]
+            if (r || x) ctx[k][1] = v[0]
+            if (b || y) ctx[k][2] = v[1] || v[0]
+            if (l || x) ctx[k][3] = v[1] || v[0]
+        }
+        return ctx[k].join(' ')
     }
-    return ctx[k].join(' ')
 }
 
 export default resolver
