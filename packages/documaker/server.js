42e9623dd4c10771cc134db7e4111b4196ccd6ec
Илья Трикоз <i.trikoz@abr-region.ru>
2020-03-23 17:48:46 +0300

    PORT

--- packages/documaker/server.js
+++ packages/documaker/server.js
@@ -2,7 +2,7 @@ const compression = require('compression')
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.port || '3000'
+const port = process.env.PORT || '3000'
 
 app.use(compression())
 app.use(express.static(__dirname))
