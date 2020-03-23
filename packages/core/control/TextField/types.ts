cc4e88db47e1b47d27ba3dc50310d0b53954d328
Dmitry Beltyukov <dm.belt@icloud.com>
2020-03-23 17:53:53 +0300

    TextField props description

--- packages/core/control/TextField/types.ts
+++ packages/core/control/TextField/types.ts
@@ -23,39 +23,113 @@ declare namespace TextFieldTypes {
     }
 
     interface InputProps {
+        /**
+         * 	Hint for form autofill feature
+         */
         autoComplete?: string
+        /**
+         * Automatically focus the form control when the page is loaded
+         */
         autoFocus?: boolean
+        /**
+         * Value of the id attribute of the <datalist> of autocomplete options
+         */
         list?: string
+        /**
+         * Name of the input form control. Submitted with the form as part of a name/value pair
+         */
         name?: string
+        /**
+         * Content to be appear in the form control when the form control is empty
+         */
         placeholder?: string
+        /**
+         * 	Pattern the value must match to be valid
+         */
         pattern?: string
+        /**
+         * The value is not editable
+         */
         readOnly?: boolean
+        /**
+         * A value is required for the form to be submittable
+         */
         required?: boolean
+        /**
+         * Type of input form control
+         */
         type?: InputTypes
+        /**
+         * Current value of the form control
+         */
         value?: string | number
 
+        /**
+         * Associates the control with a form element
+         */
         form?: string
+        /**
+         * 	URL to use for form submission
+         */
         formAction?: string
+        /**
+         * Form data set encoding type to use for form submission
+         */
         formEncType?: string
+        /**
+         * 	HTTP method to use for form submission
+         */
         formMethod?: string
+        /**
+         *  Bypass form control validation for form submission
+         */
         formNoValidate?: boolean
+        /**
+         * Browsing context for form submission
+         */
         formTarget?: string
 
+        /**
+         * Maximum value
+         */
         max?: number | string
+        /**
+         * Maximum length (number of characters) of value
+         */
         maxLength?: number
+        /**
+         * 	Minimum value
+         */
         min?: number | string
+        /**
+         * Minimum length (number of characters) of value
+         */
         minLength?: number
+        /**
+         * 	Incremental values that are valid
+         */
         step?: number | string
-        width?: number | string
     }
 
     interface TextAreaProps {
+        /**
+         * The visible width of the text control, in average character widths
+         */
         cols?: number
+        /**
+         * The number of visible text lines for the control
+         */
         rows?: number
+        /**
+         * Indicates how the control wraps text
+         */
         wrap?: string
     }
 
     interface Styles extends FieldTypes.Styles {
+        /**
+         * TextField's input
+         */
         input: {
             multiline: boolean
             size: WhaleTypes.Size
