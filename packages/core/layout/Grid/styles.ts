fb1a439de05531bcd0719131e03a7329c0880529
Dmitry Beltyukov <dm.belt@icloud.com>
2020-03-24 14:43:32 +0300

    Grid styles fix

--- packages/core/layout/Grid/styles.ts
+++ packages/core/layout/Grid/styles.ts
@@ -9,23 +9,6 @@ const styles: WhaleTypes.CreateStyles<Types.Styles, Types.Props> = (props, theme
             {
                 position: 'relative',
                 display: props.inline ? 'inline-grid' : 'grid',
-
-                gridTemplateColumns: props.templateColumns,
-                gridTemplateRows: props.templateRows,
-                gridTemplateAreas: props.templateAreas,
-
-                gridColumnGap: props.columnGap,
-                gridRowGap: props.rowGap,
-                gridGap: props.gap,
-
-                gridAutoColumns: props.autoColumns,
-                gridAutoRows: props.autoRows,
-                gridAutoFlow: props.autoFlow,
-
-                alignItems: props.alignItems,
-                alignContent: props.alignContent,
-                justifyContent: props.justifyContent,
-                justifyItems: props.justifyItems,
             },
         ]
     }
