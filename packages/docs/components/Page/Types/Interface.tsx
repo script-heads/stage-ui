8993f76ab9906d271311951b3545781a21d4b33c
Dmitry Beltyukov <dm.belt@icloud.com>
2020-04-01 11:40:10 +0300

    Interface sortTypes

--- packages/docs/components/Page/Types/Interface.tsx
+++ packages/docs/components/Page/Types/Interface.tsx
@@ -9,71 +9,56 @@ interface InterfaceProps {
     columns: TableTypes.TableColumn<Property>[]
 }
 
-// const sortTypes = (data: InterfaceType, separatedTypes?: string[]) => {
-//     const self: InterfaceType = Object.assign({}, data)
-//     const separated: Record<string, InterfaceType> = {}
-
-//     const getExtendedTypes = (type: InterfaceType, parent?: string) => {
-//         type.extendedProps.map(innerType => {
-//             if (separatedTypes && separatedTypes.includes(innerType.name) || parent) {
-//                 if (parent) {
-//                     separated[parent].props = separated[parent].props.concat(innerType.props)
-//                 } else {
-//                     separated[innerType.name] = innerType
-//                 }
-//                 getExtendedTypes(innerType, parent || innerType.name)
-//             } else {
-//                 self.props = self.props.concat(innerType.props)
-//                 getExtendedTypes(innerType)
-//             }
-//         })
-//     }
-
-//     getExtendedTypes(self)
-
-//     return { self, separated }
-// }
-
-const getAllInterfaces = (types: InterfaceType) => {
-    const extendedInterfaces: InterfaceType[] = [types]
-    const fillExtendedTypes = (types: InterfaceType[]) => {
-        for (const extendedType of types) {
-            if (extendedType.extendedTypes.length === 0) {
-                extendedInterfaces.push(extendedType)
-            }
-            fillExtendedTypes(extendedType.extendedTypes)
-        }
+const sortTypes = (data: InterfaceType, separatedTypes?: string[]) => {
+    let types: Record<string, Property[]> = {
+        [data.name]: data.types
     }
-    fillExtendedTypes(types.extendedTypes)
-    return extendedInterfaces
+
+    const getExtendedTypes = (type: InterfaceType, parent?: string) => {
+        type.extendedTypes.map(innerType => {
+            const innerName = innerType.tags.name.trim()
+            if (separatedTypes && separatedTypes.includes(innerName) || parent) {
+                if (parent) {
+                    types[parent] = types[parent].concat(innerType.types)
+                } else {
+                    types[innerName] = innerType.types
+                }
+                getExtendedTypes(innerType, parent || innerName)
+            } else {
+                types[data.name] = types[data.name].concat(innerType.types)
+                getExtendedTypes(innerType)
+            }
+        })
+    }
+
+    getExtendedTypes(data)
+
+    return types
 }
 
 const Interface = (props: InterfaceProps) => {
-    
-    const [active, setActive] = useState(props.data.id)
 
-    // const { self, separated } = useMemo(() => {
-    //     setActive(props.data.name)
-    //     return sortTypes(props.data, props.separatedTypes)
-    // }, [props])
+    const [active, setActive] = useState(props.data.name)
 
-    // const extendedNames = [props.data.name, ...Object.keys(separated)]
+    const types = useMemo(() => {
+        setActive(props.data.name)
+        return sortTypes(props.data, props.separatedTypes)
+    }, [props])
 
-    const allInterfaces = getAllInterfaces(props.data)
-    const types = allInterfaces.find(item => item.id === active)
+    const names = Object.keys(types)
 
     return (
         <Block>
             <div style={{ position: 'relative', paddingBottom: '0.5rem', marginBottom: '0.5rem' }}>
                 <ScrollView size="xs">
                     <Flexbox>
-                        {allInterfaces.map(item => (
+                        {names.map(name => (
                             <Header
-                                key={item.id}
+                                key={name}
                                 mr="1.5rem"
-                                children={item.name}
-                                color={item.id === active ? 'onBackground' : 'light'}
-                                onClick={() => setActive(item.id)}
+                                children={name}
+                                color={name === active ? 'onBackground' : 'light'}
+                                onClick={() => setActive(name)}
                             />
                         ))}
                     </Flexbox>
@@ -81,7 +66,7 @@ const Interface = (props: InterfaceProps) => {
             </div>
             <Table
                 columns={props.columns}
-                data={types?.types || []}
+                data={types[active] || []}
             />
         </Block>
     )
