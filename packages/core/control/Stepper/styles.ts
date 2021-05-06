2289a561f731b7a0a8a9fd39e6ad343398fe76c7
Awesome <jleed@me.com>
2021-05-06 20:13:36 +0300

    <Stepper /> added

---
+++ packages/core/control/Stepper/styles.ts
@@ -0,0 +1,65 @@
+import SystemTypes from '@stage-ui/system/types'
+import Types from './types'
+
+const styles: SystemTypes.CreateStyles<Types.Styles, Types.Props> = (props, theme) => {
+    return {
+        container: [
+            {
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',    
+                flex: 1,
+                transition: 'all 0.25s'
+            },
+        ],
+        step: (variant) => [
+            {
+                position: 'relative',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '2.5rem',
+                height: '2.5rem',
+                border: '1px solid',
+                color: theme.color.light.string(),
+                borderColor: theme.color.lightest.string(),
+                backgroundColor: theme.color.surface.string(),
+                borderRadius: '100rem',
+                userSelect: 'none',
+            },
+            variant({
+                active: {
+                    color: theme.color.primary.string(),
+                    borderColor: theme.color.primary.alpha(0.5).string(),    
+                },
+                complete: {
+                    color: theme.color.onPrimary.string(),
+                    backgroundColor: theme.color.primary.string(), 
+                    borderColor: theme.color.primary.string(), 
+                },
+                pointer: {
+                    cursor: 'pointer',
+                },
+            })
+        ],
+        separator: (variant) => [
+            {
+                position: 'relative',
+                flex: 1,
+                height: '0.125rem',
+                background: theme.color.lightest.string(),
+            },
+            variant({
+                active: {
+                    background: theme.color.light.string(),    
+                },
+                complete: {
+                    background: theme.color.primary.alpha(0.5).string(),    
+                },
+            })
+        ]
+    }
+}
+
+export default styles
---
+++ packages/docs/pages/docs/Control/Stepper/index.case.tsx
@@ -0,0 +1,9 @@
+import code from './default.raw'
+
+export const title = 'Stepper'
+export const subtitle= 'Basic inline stepper component'
+
+export const ns = 'StepperTypes'
+export const cases = [
+    { label: 'Basic', code },
+]
