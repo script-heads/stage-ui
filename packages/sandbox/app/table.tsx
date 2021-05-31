4b84e90a63af4fd8a01a0987e61eed45e3a6ff58
Dmitry Podkopaev <dev.elatero@gmail.com>
2021-05-31 17:36:14 +0300

    added dynamic data table

--- packages/sandbox/app/table.tsx
+++ packages/sandbox/app/table.tsx
@@ -1,46 +1,91 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
-import React from 'react'
-import { Flexbox, Table, notify } from '@stage-ui/core'
+import React, {useEffect, useState} from 'react'
+import { Flexbox, Table, notify, Spinner, Select} from '@stage-ui/core'
+import SelectTypes from '../../core/control/Select/types'
+import axios, { AxiosResponse } from 'axios'
+import { ResponsePassenger, PassengersData } from './types'
 
 type DataRow = {
   color: string
 }
 
+const endPoint = 'https://api.instantwebtools.net/v1'
+
 const PlaygroundTable = () => {
 
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<PassengersData[]>([])
+  const [pageSize, setPageSize] = useState<SelectTypes.Option[]>([{value: 10, text: "10"}])
+  
+
+  const pages = async () => {
+    setLoading(true)
+		const { data }: AxiosResponse<ResponsePassenger> = await axios({method: "GET", url: `${endPoint}/passenger`, params: {page: 0, size: pageSize[0].value} })
+    
+    setData(data.data)
+    setLoading(false)
+	}
+
+	useEffect(() => {
+		pages()
+	},[pageSize])
+
   return (
-    <Table
-      onChange={(data) => {
-        notify({
-          title: 'Row chage event',
-          message: JSON.stringify(data),
-          timeout: 3000,
-        })
-      }}
-      columns={[
-        {
-          key: 'color',
-          title: 'Color',
-          sort: 'DESC'
-        },
-        {
-          key: '',
-          dnd: (source, target, data) => {
-            notify({
-              title: 'Row dnd event fired',
-              message: `Row ${source} moved to ${target}`,
-              timeout: 3000,
-            })
+    <Flexbox direction="column">
+      <Table
+        key={loading.toString()}
+        onChange={(data) => {
+          notify({
+            title: 'Row chage event',
+            message: JSON.stringify(data),
+            timeout: 3000,
+          })
+        }}
+        columns={[
+          {
+            key: 'name',
+            title: 'Name',
+            render: (ctx) => (<Flexbox>{ctx.row.name}</Flexbox>)
           },
-        },
-      ]}
-      data={[
-        { color: 'Green' },
-        { color: 'Yellow' },
-        { color: 'Red' },
-      ]}
-    />
+          {
+            key: '',
+            dnd: (source, target, data) => {
+              notify({
+                title: 'Row dnd event fired',
+                message: `Row ${source} moved to ${target}`,
+                timeout: 3000,
+              })
+            },
+          },
+        ]}
+        data={data}
+        pagination={{ pageSize: pageSize[0].value }}
+        
+        footer={
+          loading ? (
+            <Flexbox justifyContent="center" w="100%">
+              <Spinner size="xl" mx="auto"/>
+            </Flexbox>
+          ) : undefined
+        }
+      />
+      <Flexbox>
+        <Select 
+          values={pageSize} 
+          options={[
+            {value: 5, text: '5'},
+            {value: 10, text: '10'},
+            {value: 15, text: '15'},
+            {value: 20, text: '20'},
+          ]}
+          onChange={(item) => setPageSize(item)}
+          css={{
+            maxWidth: "10rem"
+          }}
+        />
+      </Flexbox>
+    </Flexbox>
   )
 }
 
