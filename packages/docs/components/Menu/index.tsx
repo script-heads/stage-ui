fba7d3e9057147ba6382270ad980809f53e3172f
Awesome <jleed@me.com>
2021-09-13 10:29:06 +0300

    add mobile menu

--- packages/docs/components/Menu/index.tsx
+++ packages/docs/components/Menu/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Block, Button, Flexbox, Link, Text, useTheme } from '@stage-ui/core'
-import { Brush, Github } from '@stage-ui/icons'
+import { Brush, Github, Menu as Burger } from '@stage-ui/icons'
 import ButtonTypes from '@stage-ui/core/control/Button/types'
 import { panel } from '@stage-ui/docs/components/Themer/src/ThemeConfigurator'
 import ThemeSwitcher from './ThemeSwitcher'
@@ -9,31 +9,94 @@ import { CustomPageProps } from '../../utils/core'
 type MenuItemProps = ButtonTypes.Props & {
   active: boolean
 }
-const MenuItem = (props: MenuItemProps) => {
+const MenuItem = (props: MenuItemProps & { mobile?: boolean }) => {
   const { active, ...buttomProps } = props
   return (
     <Button
       p="0"
       mr="m"
+      size="xl"
       decoration="text"
-      color={props.active ? 'onSurface' : 'light'}
+      color={
+        // eslint-disable-next-line no-nested-ternary
+        props.active ? (props.mobile ? 'surface' : 'onSurface') : props.mobile ? 'hardest' : 'light'
+      }
       overrides={{
-        container: () => [
-          {
-            transition: 'all 0.125s',
-            background: 'none !important',
-          },
-        ],
+        container: {
+          transition: 'all 0.125s',
+          fontWeight: 'bold',
+          background: 'none !important',
+        },
       }}
       {...buttomProps}
     />
   )
 }
 
+const MenuItems = (props: CustomPageProps & { onCloseMobileMenu?: () => void }) => {
+  return (
+    <>
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Getting started"
+        active={props.path === '/getting-started'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/getting-started')
+        }}
+      />
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Props"
+        active={props.path === '/props'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/props')
+        }}
+      />
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Overrides"
+        active={props.path === '/overrides'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/overrides')
+        }}
+      />
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Theming"
+        active={props.path === '/theming'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/theming')
+        }}
+      />
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Icons"
+        active={props.path === '/icons'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/icons')
+        }}
+      />
+      <MenuItem
+        mobile={!!props.onCloseMobileMenu}
+        label="Components"
+        active={props.path === '/components'}
+        onClick={() => {
+          props.onCloseMobileMenu?.()
+          props.setPath('/components')
+        }}
+      />
+    </>
+  )
+}
 const Menu = (props: CustomPageProps) => {
   const landingPage = props.path === '/'
   const theme = useTheme()
-
+  const [mobileMenu, setMobileMenu] = useState(false)
   if (landingPage) {
     return null
   }
@@ -42,41 +105,15 @@ const Menu = (props: CustomPageProps) => {
     <Flexbox w="100%" column pb="xl">
       <Flexbox justifyContent="space-between" alignItems="center">
         {/* <Block mr="xl">
-          <Text size="m" weight={800} onClick={() => props.setPath('/')}>
-            {props.config.name}
+          <Text size="m" weight={800} onClick={() => {
+            props.onCloseMobileMenu?.()
+            props.setPath('/')}>
+            {props.config.nam
+            }e}
           </Text>
         </Block> */}
-        <Flexbox flex={1}>
-          <MenuItem
-            label="Getting started"
-            active={props.path === '/getting-started'}
-            onClick={() => props.setPath('/getting-started')}
-          />
-          <MenuItem
-            label="Props"
-            active={props.path === '/props'}
-            onClick={() => props.setPath('/props')}
-          />
-          <MenuItem
-            label="Overrides"
-            active={props.path === '/overrides'}
-            onClick={() => props.setPath('/overrides')}
-          />
-          <MenuItem
-            label="Theming"
-            active={props.path === '/theming'}
-            onClick={() => props.setPath('/theming')}
-          />
-          <MenuItem
-            label="Icons"
-            active={props.path === '/icons'}
-            onClick={() => props.setPath('/icons')}
-          />
-          <MenuItem
-            label="Components"
-            active={props.path === '/components'}
-            onClick={() => props.setPath('/components')}
-          />
+        <Flexbox flex={1} display={['flex', 'flex', 'none']}>
+          <MenuItems {...props} />
         </Flexbox>
         <Flexbox>
           <ThemeSwitcher
@@ -102,6 +139,40 @@ const Menu = (props: CustomPageProps) => {
             </Link>
           )}
         </Flexbox>
+        <Flexbox display={['none', 'none', 'flex']} css={{ zIndex: 101 }}>
+          <Burger
+            size="1.5rem"
+            shape="circle"
+            backgroundColor="onSurface"
+            color="surface"
+            onClick={() => {
+              setMobileMenu(!mobileMenu)
+            }}
+          />
+        </Flexbox>
+        <Block
+          position="fixed"
+          p="xl"
+          backgroundColor={theme.color.onSurface}
+          css={{
+            width: '100vw',
+            height: '100vh',
+            top: 0,
+            left: 0,
+            zIndex: 100,
+            visibility: mobileMenu ? 'visible' : 'hidden',
+            opacity: mobileMenu ? '1' : '0',
+            transform: mobileMenu ? 'scale(1)' : 'scale(1.05)',
+            transition: 'all 0.125s ease-in-out;',
+          }}
+        >
+          <MenuItems
+            {...props}
+            onCloseMobileMenu={() => {
+              setMobileMenu(false)
+            }}
+          />
+        </Block>
       </Flexbox>
     </Flexbox>
   )
