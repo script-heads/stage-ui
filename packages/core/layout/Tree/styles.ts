abd2a8c427b2c20ee776f5fda1ee37b6c1903027
Dmitry Beltyukov <m@dmit.be>
2021-09-14 21:26:50 +0300

    remove variants complete

--- packages/core/layout/Tree/styles.ts
+++ packages/core/layout/Tree/styles.ts
@@ -2,61 +2,45 @@ import Types from './types'
 
 const createClasses: Stage.CreateClasses<Types.Classes, Types.PrivateProps> = (theme, props) => {
   return {
-    container: (state, variant) => [
-      variant({
-        decoration: {
-          finder: {
-            '>div:nth-of-type(even)': {
-              background: theme.color.background.rgb().string(),
-            },
-          },
+    container: (state) => [
+      state.decoration === 'finder' && {
+        '>div:nth-of-type(even)': {
+          background: theme.color.background.rgb().string(),
         },
-      }),
+      },
     ],
-    row: (state, variant) => [
-      variant({
-        decoration: {
-          finder: [
-            {
-              padding: `calc(${theme.spacing.m} / 2)`,
-            },
-            variant({
-              size: {
-                xs: {
-                  padding: `calc(${theme.spacing.xs} / 2)`,
-                },
-                s: {
-                  padding: `calc(${theme.spacing.s} / 2)`,
-                },
-                l: {
-                  padding: `calc(${theme.spacing.l} / 2)`,
-                },
-                xl: {
-                  padding: `calc(${theme.spacing.xl} / 2)`,
-                },
-              },
-            }),
-          ],
+    row: (state) => [
+      state.decoration === 'finder' && [
+        {
+          padding: `calc(${theme.spacing.m} / 2)`,
         },
-      }),
+        state.size === 'xs' && {
+          padding: `calc(${theme.spacing.xs} / 2)`,
+        },
+        state.size === 's' && {
+          padding: `calc(${theme.spacing.s} / 2)`,
+        },
+        state.size === 'l' && {
+          padding: `calc(${theme.spacing.l} / 2)`,
+        },
+        state.size === 'xl' && {
+          padding: `calc(${theme.spacing.xl} / 2)`,
+        },
+      ],
     ],
 
-    label: (state, variant) => [
+    label: (state) => [
       {
         cursor: 'pointer',
         userSelect: 'none',
         flex: 1,
       },
-      variant({
-        decoration: {
-          finder: {
-            fontWeight: 500,
-          },
-        },
-      }),
+      state.decoration === 'finder' && {
+        fontWeight: 500,
+      },
     ],
 
-    arrow: (state, variant) => [
+    arrow: (state) => [
       {
         opacity: 0,
         cursor: 'pointer',
@@ -66,45 +50,39 @@ const createClasses: Stage.CreateClasses<Types.Classes, Types.PrivateProps> = (t
         paddingRight: `calc(${theme.spacing.m} / 2)`,
         paddingLeft: `calc(${theme.assets.typography.text.m.fontSize} * ${props.lvl * 1})`,
       },
-      variant({
-        hasChilds: {
-          opacity: 1,
-        },
-        size: {
-          xs: {
-            fontSize: theme.assets.typography.text.xs.fontSize,
-            paddingRight: `calc(${theme.spacing.xs} / 2)`,
-            paddingLeft: `calc(${theme.assets.typography.text.xs.fontSize} * ${props.lvl * 1})`,
-          },
-          s: {
-            fontSize: theme.assets.typography.text.s.fontSize,
-            paddingRight: `calc(${theme.spacing.s} / 2)`,
-            paddingLeft: `calc(${theme.assets.typography.text.s.fontSize} * ${props.lvl * 1})`,
-          },
-          l: {
-            fontSize: theme.assets.typography.text.l.fontSize,
-            paddingRight: `calc(${theme.spacing.l} / 2)`,
-            paddingLeft: `calc(${theme.assets.typography.text.l.fontSize} * ${props.lvl * 1})`,
-          },
-          xl: {
-            fontSize: theme.assets.typography.text.xl.fontSize,
-            paddingRight: `calc(${theme.spacing.xl} / 2)`,
-            paddingLeft: `calc(${theme.assets.typography.text.xl.fontSize} * ${props.lvl * 1})`,
-          },
-        },
-      }),
+      state.hasChilds && {
+        opacity: 1,
+      },
+      state.size === 'xs' && {
+        fontSize: theme.assets.typography.text.xs.fontSize,
+        paddingRight: `calc(${theme.spacing.xs} / 2)`,
+        paddingLeft: `calc(${theme.assets.typography.text.xs.fontSize} * ${props.lvl * 1})`,
+      },
+      state.size === 's' && {
+        fontSize: theme.assets.typography.text.s.fontSize,
+        paddingRight: `calc(${theme.spacing.s} / 2)`,
+        paddingLeft: `calc(${theme.assets.typography.text.s.fontSize} * ${props.lvl * 1})`,
+      },
+      state.size === 'l' && {
+        fontSize: theme.assets.typography.text.l.fontSize,
+        paddingRight: `calc(${theme.spacing.l} / 2)`,
+        paddingLeft: `calc(${theme.assets.typography.text.l.fontSize} * ${props.lvl * 1})`,
+      },
+      state.size === 'xl' && {
+        fontSize: theme.assets.typography.text.xl.fontSize,
+        paddingRight: `calc(${theme.spacing.xl} / 2)`,
+        paddingLeft: `calc(${theme.assets.typography.text.xl.fontSize} * ${props.lvl * 1})`,
+      },
     ],
-    child: (state, variant) => [
+    child: (state) => [
       {
         display: 'none',
       },
-      variant({
-        isOpen: [
-          {
-            display: 'block',
-          },
-        ],
-      }),
+      state.isOpen && [
+        {
+          display: 'block',
+        },
+      ],
     ],
   }
 }
