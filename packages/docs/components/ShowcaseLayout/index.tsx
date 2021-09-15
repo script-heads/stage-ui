78a09d2f43156d91c69b4b38e0e36196ec1c580d
Awesome <jleed@me.com>
2021-09-15 12:51:30 +0300

    add find

--- packages/docs/components/ShowcaseLayout/index.tsx
+++ packages/docs/components/ShowcaseLayout/index.tsx
@@ -1,25 +1,59 @@
-import { Block, Flexbox } from '@stage-ui/core'
+import { Block, Flexbox, TextField } from '@stage-ui/core'
 import { CustomPageProps } from '@stage-ui/docs/utils/core'
-import React from 'react'
+import { Search } from '@stage-ui/icons'
+import React, { useState } from 'react'
 import Item from './Item'
 
 const ShowcaseLayout = (props: CustomPageProps) => {
+  const [search, setSearch] = useState('')
+
   return (
-    <Flexbox column alignItems="flex-start" pb="4rem" css={{ gap: '1rem' }}>
-      {Object.keys(props.pages).map((pageCode) => {
-        return (
-          <Block key={pageCode}>
-            {/* <Text size="s" color="light" weight={600} pl="xl">
+    <Flexbox column>
+      <TextField
+        placeholder="Find"
+        w="12rem"
+        mb="l"
+        rightChild={<Search />}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Flexbox column alignItems="flex-start" pb="4rem" css={{ gap: '1rem' }}>
+        {Object.keys(props.pages)
+          .filter((pk) => {
+            if (search) {
+              return (
+                props.pages[pk].findIndex((page) => {
+                  if (RegExp(search.toLowerCase()).exec(page.title.toLowerCase())) {
+                    return true
+                  }
+                  return false
+                }) >= 0
+              )
+            }
+            return true
+          })
+          .map((pageCode) => {
+            return (
+              <Block key={pageCode}>
+                {/* <Text size="s" color="light" weight={600} pl="xl">
               {pageCode}
             </Text> */}
-            <Flexbox wrap="wrap" css={{ gap: '1rem' }}>
-              {props.pages[pageCode].map((item) => {
-                return <Item key={item.id} page={item} setPath={props.setPath} />
-              })}
-            </Flexbox>
-          </Block>
-        )
-      })}
+                <Flexbox wrap="wrap" css={{ gap: '1rem' }}>
+                  {props.pages[pageCode]
+                    .filter((key) => {
+                      if (!!search && !RegExp(search.toLowerCase()).exec(key.title.toLowerCase())) {
+                        return false
+                      }
+                      return true
+                    })
+                    .map((item) => {
+                      return <Item key={item.id} page={item} setPath={props.setPath} />
+                    })}
+                </Flexbox>
+              </Block>
+            )
+          })}
+      </Flexbox>
     </Flexbox>
   )
 }
