b71dfa1b6a318f36d852ff47efb486328f497ab2
Dmitry Podkopaev <dev.elatero@gmail.com>
2021-10-04 17:40:29 +0300

    fixed layout text textArea

--- packages/core/control/TextField/styles.ts
+++ packages/core/control/TextField/styles.ts
@@ -3,48 +3,46 @@ import Types from './types'
 const styles: Stage.CreateClasses<Types.Classes, Types.Props> = (
   theme,
   { lineHeight, size = 'm', leftChildNumber },
-) => {
-  return {
-    input: (state) => [
-      {
-        outline: 0,
-        padding: 0,
-        margin: 0,
-        border: 'none',
-        backgroundImage: 'none',
-        backgroundColor: 'transparent',
-        resize: 'vertical',
-        boxShadow: 'none',
-        color: 'inherit',
-        font: 'inherit',
-        cursor: 'inherit',
-        textOverflow: 'ellipsis',
-        '&::placeholder': {
-          userSelect: 'none',
-          color: theme.color.light.rgb().string(),
-        },
+) => ({
+  input: (state) => [
+    {
+      outline: 0,
+      padding: 0,
+      margin: 0,
+      border: 'none',
+      backgroundImage: 'none',
+      backgroundColor: 'transparent',
+      resize: 'vertical',
+      boxShadow: 'none',
+      color: 'inherit',
+      font: 'inherit',
+      cursor: 'inherit',
+      textOverflow: 'ellipsis',
+      '&::placeholder': {
+        userSelect: 'none',
+        color: theme.color.light.rgb().string(),
       },
-      state.multiline && {
-        padding: theme.assets.field[size].indent,
-        paddingLeft: leftChildNumber ? 0 : undefined,
+    },
+    state.multiline && {
+      padding: theme.assets.field[size].indent,
+      paddingLeft: leftChildNumber ? 0 : undefined,
+    },
+    state.disabled && {
+      color: theme.color.hardest.rgb().string(),
+    },
+  ],
+  lineNumbers: [
+    {
+      backgroundColor: theme.color.lightest.hex(),
+      height: 'max-content',
+      position: 'absolute',
+      padding: `${theme.assets.field[size].indent}`,
+      textAlign: 'right',
+      '> div': {
+        height: lineHeight || theme.assets.typography.text[size].lineHeight,
       },
-      state.disabled && {
-        color: theme.color.hardest.rgb().string(),
-      },
-    ],
-    lineNumbers: [
-      {
-        backgroundColor: theme.color.lightest.hex(),
-        height: '100%',
-        position: 'absolute',
-        padding: `${theme.assets.field[size].indent}`,
-        textAlign: 'right',
-        '> div': {
-          height: lineHeight || theme.assets.typography.text[size].lineHeight,
-        },
-      },
-    ],
-  }
-}
+    },
+  ],
+})
 
 export default styles
