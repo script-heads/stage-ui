4e6906f391dbc95b0520b158e1ab3797d6f2b40e
Ilya T <jleed@me.com>
2022-01-14 07:33:50 +0300

    fix deploy

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-export NAMESPACE="$DRONE_DEPLOY_TO"
-export VERSION=v$(node -p -e "require('./package.json').version").$DRONE_BUILD_PARENT
+# export NAMESPACE="$DRONE_DEPLOY_TO"
+# export VERSION=v$(node -p -e "require('./package.json').version").$DRONE_BUILD_PARENT
+
+# Hardcode because we deploy this project right after build complete
+export NAMESPACE="stageui"
+# Not parent because we deploy this project right after build complete
+export VERSION=v$(node -p -e "require('./package.json').version").$DRONE_BUILD_NUMBER
 
 echo $KUBE_CONFIG | base64 -d > kubeconfig.yml
 
 echo "Deploy version '$VERSION' to '$NAMESPACE'"
 
-awk 'FNR==1{print "---"}{print}' ./deploy/manifests/*.yaml | \
+awk 'FNR==1{print "---"}{print}' ./kube/manifests/*.yaml | \
 sed "s/_NAMESPACE/$NAMESPACE/g" | \
 sed "s/_VERSION/$VERSION/g" | \
 kubectl --kubeconfig=kubeconfig.yml apply -f  -
