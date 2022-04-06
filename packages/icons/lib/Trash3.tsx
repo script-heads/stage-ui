25abe4b6a5b8b1a88bbcd62252695855b8ac2b67
Ilya T <jleed@me.com>
2022-04-06 08:18:55 +0300

    new icons added

---
+++ packages/icons/lib/Trash3.tsx
@@ -0,0 +1,12 @@
+/**
+The MIT License (MIT)
+*/
+import{ Props, createIcon } from '../Icon'
+import React from 'react'
+
+export default React.forwardRef((props: Props, ref: React.ForwardedRef<HTMLSpanElement>) => (
+    createIcon(props, ref, {
+        filled: <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM19 4H15.5L14.5 3H9.5L8.5 4H5V6H19V4Z"/>,
+        outline: <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z"/>,
+    })
+))
