5526aac1ecbb73692585476cf3c6479eb875962e
Dmitry Beltyukov <m@dmit.be>
2022-04-07 13:48:51 +0300

    fix resolve html named color in colorProp

--- packages/system/props/color.ts
+++ packages/system/props/color.ts
@@ -5,6 +5,24 @@ export type ColorProp =
   | Color
   | Stage.ColorNames
 
+const shadeColors = [
+  'gray',
+  'yellow',
+  'orange',
+  'red',
+  'rose',
+  'pink',
+  'green',
+  'lightGreen',
+  'lime',
+  'teal',
+  'cyan',
+  'lightBlue',
+  'blue',
+  'indigo',
+  'purple',
+]
+
 function colorProp<V extends ColorProp | undefined>(
   value: V,
   theme: Stage.Theme,
@@ -13,26 +31,31 @@ function colorProp<V extends ColorProp | undefined>(
 
   switch (typeof value) {
     case 'string': {
-      if (Object.hasOwnProperty.call(theme.color, value)) {
-        resolvedColor = theme.color[value as keyof Stage.Colors]
-        break
-      }
-      if (Object.hasOwnProperty.call(theme.color.palette, value)) {
-        resolvedColor = theme.color.palette[value]
-        break
-      }
       const [color, shade] = value.split(/(\d+)/) as [
         keyof Stage.Colors,
         keyof Stage.ColorShades,
       ]
-      if (Object.hasOwnProperty.call(theme.color, color)) {
-        if (shade && Object.hasOwnProperty.call(theme.color[color], shade)) {
-          resolvedColor = (theme.color[color] as Stage.ColorShades)[shade]
-          break
-        }
-        resolvedColor = theme.color[color]
+
+      const isShadeColor =
+        shade &&
+        shadeColors.includes(color) &&
+        Object.hasOwnProperty.call(theme.color[color], shade)
+
+      if (isShadeColor) {
+        resolvedColor = (theme.color[color] as Stage.ColorShades)[shade]
         break
       }
+
+      if (!isShadeColor && Object.hasOwnProperty.call(theme.color, value)) {
+        resolvedColor = theme.color[value as keyof Stage.Colors]
+        break
+      }
+
+      if (Object.hasOwnProperty.call(theme.color.palette, value)) {
+        resolvedColor = theme.color.palette[value]
+        break
+      }
+
       resolvedColor = Color(value)
       break
     }
