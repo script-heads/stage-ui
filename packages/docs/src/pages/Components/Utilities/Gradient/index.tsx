c8bae5e9074af3a08131d628b578e009dfd2f68e
Ilya T <jleed@me.com>
2023-02-19 12:12:25 +0300

    gradient docs

--- packages/docs/src/pages/Components/Utilities/Gradient/index.tsx
+++ packages/docs/src/pages/Components/Utilities/Gradient/index.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
-import { Block, Paragraph } from '@stage-ui/core'
+import { Block, Header, Paragraph } from '@stage-ui/core'
 
 import basic from './basic.raw?raw'
 import angle from './angle.raw?raw'
@@ -18,10 +19,35 @@ export const cases = [
 
 export default () => (
   <Block p="m">
+    <Paragraph mt="m">
+      If you're building a website or web application, you may want to add some visual
+      flair with gradients. Gradients are a way to create a smooth transition between two
+      or more colors, giving your design depth and texture. This utility generates CSS
+      gradients from input variables, making it easy to customize your gradients and reuse
+      them throughout your project.
+    </Paragraph>
+    <Paragraph mt="m">
+      The utility can take a variety of input variables to generate a gradient, including
+      colors, position angle, gradient type, and whether the gradient should repeat.
+    </Paragraph>
+    <Header size="s"> Here's an overview of the options:</Header>
     <Paragraph>
-      This utility allows you to create custom gradients from colors of the theme. You can
-      use it to create a custom gradient palette in the theme or create complex gradients
-      right in the component.
+      colors - The utility can take an array of colors as input, and will create a
+      gradient that transitions between them. Colors also can be Stage.Color from active
+      theme on Viewport.
+    </Paragraph>
+    <Paragraph mt="m">
+      angle - This option sets the angle of the gradient. It can be either a number (which
+      will be converted to a percentage), or a string that includes a unit of measurement
+      such as "px" or "rem". This option only works for linear gradients. pattern.
+    </Paragraph>
+    <Paragraph mt="m">
+      type - There are three types of gradients that the utility can generate: linear,
+      radial, and conic. By default, the utility will generate a linear gradient.
+    </Paragraph>
+    <Paragraph mt="m">
+      repeating - If this option is set to true, the gradient will repeat itself, creating
+      a seamless
     </Paragraph>
   </Block>
 )
