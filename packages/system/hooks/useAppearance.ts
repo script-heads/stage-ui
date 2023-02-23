fafbbd60a2733b9e16aab068b5804760ccaee36e
Ilya T <jleed@me.com>
2023-02-24 01:44:23 +0300

    0.6.5 fix window for gatsby

--- packages/system/hooks/useAppearance.ts
+++ packages/system/hooks/useAppearance.ts
@@ -5,13 +5,13 @@ type Appearance = 'dark' | 'light'
 // Detects changes to the system theme and returns 'dark' or 'light'
 const useAppearance = (): Appearance => {
   const [appearance, setAppearance] = useState<Appearance>(
-    window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+    window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
   )
   useEffect(() => {
     const onChange = (e: { matches: boolean }) => {
       setAppearance(e.matches ? 'dark' : 'light')
     }
-    const watchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    const watchMedia = window?.matchMedia('(prefers-color-scheme: dark)')
     watchMedia.addEventListener('change', onChange)
     setAppearance(watchMedia.matches ? 'dark' : 'light')
     return () => {
