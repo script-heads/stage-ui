e32bb2d95c253ec826592440f93344bb99a1d12e
Ilia Trikoz <jleed@me.com>
2025-10-30 20:27:47 +0300

    0.7.55 - fix button auto contrast

--- packages/core/control/Button/styles.ts
+++ packages/core/control/Button/styles.ts
@@ -12,7 +12,7 @@ const styles: Stage.CreateClasses<Types.Classes, Types.Props> = (theme, props) =
     uppercase,
   } = props
   const color = colorProp(props.color, theme) || theme.color.primary
-  const isDark = color.contrast(theme.color.surface) > 4
+  const isDark = color.contrast(theme.color.onPrimary) > 2.4
 
   const applyDecoration = (
     decorations: Record<
@@ -70,7 +70,7 @@ const styles: Stage.CreateClasses<Types.Classes, Types.Props> = (theme, props) =
         filled: {
           idle: {
             background: color.string(),
-            color: isDark ? theme.color.surface.string() : theme.color.onSurface.string(),
+            color: isDark ? theme.color.onPrimary.string() : theme.color.black.string(),
           },
           hover: {
             background: color.whiten(0.4).hex(),
